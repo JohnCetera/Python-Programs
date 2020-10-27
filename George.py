090657261d3b2b7f161ea444ae880221912ca627
John Cetera <45797210+JohnCetera@users.noreply.github.com>
2020-10-27 04:58:50 -0500

    Delete George.py

--- George.py
+++
@@ -1,9 +0,0 @@
-character_name = "Thom"
-character_age = "50"
-is_Male = True
-print("there once was a man named " + character_name + ",")
-print("he was " + character_age + " years old")
-
-character_name = "Mike"
-print("he really like the name " + character_name + ",")
-print("but he didn't like being " + character_age + ".")
